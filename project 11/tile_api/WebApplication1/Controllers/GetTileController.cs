52ec54524b04c90697f8aa9b245250657ea0fa01
ali_karimi78 <ali0karimi1azari@gmail.com>
2023-05-07 03:42:19 +0430

    tile_api

--- project 11/tile_api/WebApplication1/Controllers/GetTileController.cs
+++ project 11/tile_api/WebApplication1/Controllers/GetTileController.cs
@@ -12,22 +12,37 @@ namespace WebApplication1.Controllers
 {
     public class GetTileController : ApiController
     {
+
         [HttpGet]
         public HttpResponseMessage GetTile(int zoom , int row , int col)
         {
-            string url_name = "http://localhost:8080/geoserver/topp/gwc/service/wmts?layer=topp%3Astates&style=&tilematrixset=EPSG%3A900913&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=EPSG%3A900913%3A6&TileCol=" + col + "&TileRow=" + row;
-            WebRequest request = WebRequest.Create(url_name);
-            var response = request.GetResponse();
-            Stream stream = response.GetResponseStream();
-            Bitmap bmp = new Bitmap(stream);
-            string tile_path = "D:\\karshenasi_arshad\\webGIS\\webGIS_ali_karimi\\project 11\\tile_download\\tile_pic1.png";
-            bmp.Save(tile_path);
-            HttpResponseMessage response_msg = new HttpResponseMessage();
-            var fs = new FileStream(tile_path, FileMode.Open);
-            response_msg.Content = new StreamContent(fs);
-            response_msg.Content.Headers.ContentType = new MediaTypeHeaderValue("Image/png");
+            try
+            {
+                string url_name = $"http://localhost:8080/geoserver/topp/gwc/service/wmts?layer=topp%3Astates&style=&tilematrixset=EPSG%3A900913&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=EPSG%3A900913%3A{zoom}&TileCol={col}&TileRow={row}";
+                WebRequest request = WebRequest.Create(url_name);
+                var response = request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                Bitmap bmp = new Bitmap(stream);
+                string tile_path = $"D:\\karshenasi_arshad\\webGIS\\webGIS_ali_karimi\\project 11\\tile_download\\tile_zoom_{zoom}_row_{row}_col_{col}.png";
+                bmp.Save(tile_path);
+                HttpResponseMessage response_msg = new HttpResponseMessage();
+                var fs = new FileStream(tile_path, FileMode.Open);
+                response_msg.Content = new StreamContent(fs);
+                response_msg.Content.Headers.ContentType = new MediaTypeHeaderValue("Image/png");
 
-            return response_msg;
+                return response_msg;
+            }
+            catch (WebException ex)
+            {
+                var errorResponse = ex.Response as HttpWebResponse;
+                var errorStream = errorResponse.GetResponseStream();
+                var errorMessage = new StreamReader(errorStream).ReadToEnd();
+                HttpResponseMessage response_msg = new HttpResponseMessage(errorResponse.StatusCode);
+                response_msg.Content = new StringContent(errorMessage);
+                response_msg.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+                return response_msg;
+            }
+          
         }
     }
 }
